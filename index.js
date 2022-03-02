8bd17769d884dedc9248a53c6024c3a8f48e60b5
Fraser Irvine <1801054@rgu.ac.uk>
2022-03-02 13:19:41 +0000

    fixed post request

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const { json } = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
 const router = express.Router();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
 
 app.set('view engine', 'ejs');
 
